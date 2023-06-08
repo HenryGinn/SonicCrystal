61db422aeb686e3e0b6c216f64b2fcde2dbd67ad
Henry Ginn <henryginn137@gmail.com>
2023-06-08 10:54:31 +0100

    File name toolkit done

--- CryostatInterface.py
+++ CryostatInterface.py
@@ -2,7 +2,7 @@
 import zhinst.core
 import time
 
-results_path = "D:\\Documents\\Python Scripts\\Scripts Henry\\CryostatInterface\\Results"
+#results_path = "D:\\Documents\\Python Scripts\\Scripts Henry\\CryostatInterface\\Results"
 
 daq = zhinst.core.ziDAQServer('192.168.103.198', 8004, 6)
 daq.setDouble('/dev6641/imps/0/freq', 10000)
@@ -13,11 +13,25 @@ sweeper.subscribe('/dev6641/imps/0/sample')
 sweeper.execute()
 sweeper.unsubscribe("*")
 #sweeper.set('save/directory', results_path)
-sweeper.set("save/saveonread", True)
-sweeper.save("bob")
+#sweeper.set("save/saveonread", True)
+sweeper.save()
 a = sweeper.read()
 print(a)
-"""
 
+"""
 from getpeak import get_peak
-get_peak(plot_results=True)
+#peak = get_peak(plot_results=True)
+
+from filenames import get_file_name
+from filenames import read_file_name
+
+input_dict = {"probe power": {"value": 26, "unit": "dBm"},
+              "cavity frequency": 39884000,
+              "detuning": 3000000}
+print("Input to file name creator:")
+print(input_dict)
+print("")
+file_name = get_file_name(input_dict)
+print(f"File name:\n{file_name}\n")
+
+print(f"Extracting from file name:\n{read_file_name(file_name)}")
